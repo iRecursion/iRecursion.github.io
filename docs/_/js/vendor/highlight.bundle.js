ed1e884a1c0bcdad47a5eebd2dfde970c1a065e3
iRecursion <2032047511@qq.com>
2024-04-14 23:29:31 +0800

    switch highlight to vs.css

--- docs/_/js/vendor/highlight.bundle.js
+++ docs/_/js/vendor/highlight.bundle.js
@@ -1,25 +1,63 @@
 ;(function () {
   'use strict'
 
-  var hljs = require('highlight.js/lib/highlight') 
+  var hljs = require('highlight.js/lib/highlight')
+  
+  /*
+  Configuration：xml，json，yaml，properties
+
+  hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+  hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
+  hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
+  hljs.registerLanguage('properties', require('highlight.js/lib/languages/properties'))
+  
+  Language：java，rust，python，sql，css，protobuf，javascript
+  
+  hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
+  hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
+  hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
+  hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'))
+  hljs.registerLanguage('python', require('highlight.js/lib/languages/python'))
+  hljs.registerLanguage('protobuf', require('highlight.js/lib/languages/protobuf'))
+  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+  
+  Rare：groovy，gradle，bash，markdown，asciidoc，dockerfile
+  
   hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
   hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'))
-  hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
-  hljs.registerLanguage('protobuf', require('highlight.js/lib/languages/protobuf'))
-  hljs.registerLanguage('ini', require('highlight.js/lib/languages/ini'))
-  hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'))
-  hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
-  hljs.registerLanguage('python', require('highlight.js/lib/languages/python'))
   hljs.registerLanguage('groovy', require('highlight.js/lib/languages/groovy'))
-  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
-  hljs.registerLanguage('ini', require('highlight.js/lib/languages/ini'))
-  hljs.registerLanguage('properties', require('highlight.js/lib/languages/properties'))
   hljs.registerLanguage('gradle', require('highlight.js/lib/languages/gradle'))
-  hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
-  hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
-  hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
+  hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
+  hljs.registerLanguage('asciidoc', require('highlight.js/lib/languages/asciidoc'))
   hljs.registerLanguage('dockerfile', require('highlight.js/lib/languages/dockerfile'))
+
+  
+  */
+  
+  hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+  hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
+  hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
+  hljs.registerLanguage('properties', require('highlight.js/lib/languages/properties'))
+  
+  hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
   hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
+  hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
+  hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'))
+  hljs.registerLanguage('python', require('highlight.js/lib/languages/python'))
+  hljs.registerLanguage('protobuf', require('highlight.js/lib/languages/protobuf'))
+  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+  
+  hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
+  hljs.registerLanguage('groovy', require('highlight.js/lib/languages/groovy'))
+  hljs.registerLanguage('gradle', require('highlight.js/lib/languages/gradle'))
+  hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
+  hljs.registerLanguage('asciidoc', require('highlight.js/lib/languages/asciidoc'))
+  hljs.registerLanguage('dockerfile', require('highlight.js/lib/languages/dockerfile'))
+  
+  hljs.registerLanguage('graphql', require('highlight.js/lib/languages/graphql'))
+  
+  
+  
   
   ;[].slice.call(document.querySelectorAll('pre code.hljs[data-lang]')).forEach(function (node) {
     hljs.highlightBlock(node)
